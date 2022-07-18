9894a24b0c67234a95c8a52794dfcc1c39fcec82
Aditya K Goyal <101977885+Aditya-K-Goyal@users.noreply.github.com>
2022-07-18 19:55:30 +0530

    Add files via upload

---
+++ tv.js
@@ -0,0 +1,31 @@
+Status="";
+img="";
+function preload()
+{
+   img = loadImage("tv.jpg");
+}
+function setup()
+{
+    canvas = createCanvas(400,400);
+    canvas.center();
+    document.getElementById("status").innerHTML="status: Objecting objects";
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+}
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    Status = true;
+    object_Detector.Detect(img,gotResults);
+}
+function draw()
+{
+    image(img,0,0,400,400);
+}
+function gotResults(error,Results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+}
